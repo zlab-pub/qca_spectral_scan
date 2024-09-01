6e21ac405a7a8f2749aa231787904314b07d0553
zeertzjq <zeertzjq@outlook.com>
2024-09-01 16:07:35 +0800

    Retrieve qca-vendor.h using CMake

--- build-libnl.sh
+++ build-libnl.sh
@@ -4,6 +4,7 @@ cd ./libnl-3.10.0/ || exit
 sed -i 's|-lpthread||g' ./configure
 export TOOLCHAIN=~/Android/Sdk/ndk/26.3.11579264/toolchains/llvm/prebuilt/linux-x86_64/
 export TARGET=aarch64-linux-android
+export ABI=arm64-v8a
 export API=24
 export AR="${TOOLCHAIN}/bin/llvm-ar"
 export CC="${TOOLCHAIN}/bin/${TARGET}${API}-clang"
@@ -12,6 +13,6 @@ export CXX="${TOOLCHAIN}/bin/${TARGET}${API}-clang++"
 export LD="${TOOLCHAIN}/bin/ld"
 export RANLIB="${TOOLCHAIN}/bin/llvm-ranlib"
 export STRIP="${TOOLCHAIN}/bin/llvm-strip"
-./configure --enable-cli=no --host ${TARGET} --prefix="${PWD}/../distribution" CFLAGS='-fPIC -Din_addr_t=uint32_t' CXXFLAGS='-fPIC -Din_addr_t=uint32_t'
+./configure --enable-cli=no --host ${TARGET} --prefix="${PWD}/../distribution" --exec-prefix="${PWD}/../distribution/${ABI}" CFLAGS='-fPIC -Din_addr_t=uint32_t' CXXFLAGS='-fPIC -Din_addr_t=uint32_t'
 make
 make install
